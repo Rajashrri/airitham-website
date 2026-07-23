93532dc931e69603d7e6adf0082ed1bdc7b2ac89
Devendramali <devendramali01234@gmail.com>
2026-07-23 18:25:25 +0530

    d

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -256,7 +256,7 @@ document.title = `${blog?.meta_title || blog?.name } | Blog Details | AIRITHM`;
               Related Blogs
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[16px] mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[16px] mx-auto mt-[40px]">
               {relatedBlogs.map((item) => (
                 <a
                   href={`/blog/${item.url}`}
